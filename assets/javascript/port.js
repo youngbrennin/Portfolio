f5f688364f438f5fc468fde367db32c33d027fd6
Michelangelo <youngbrennin20@gmail.com>
2018-07-10 00:49:50 -0700

    finished portfolio

--- assets/javascript/port.js
+++ assets/javascript/port.js
@@ -1,3 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function () {
     $('.collapsible').collapsible();
-  });
+});
+
+$(document).ready(function () {
+    $('.carousel').carousel();
+});
